5aab16f204605051d87f42a57645526351e98fee
Roxanneyin <Roxanneyin@users.noreply.github.com>
2017-08-24 14:17:23 -0400

    added

--- Euler.py
+++ Euler.py
@@ -9,3 +9,21 @@ def is_prime(n):
             return False
         p += 2
     return True
+
+def is_palindrom(n):
+    n = str(n)
+    if n[::-1] == n:
+        return True
+    else:
+        return False
+
+def foo(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return -3
+    else:
+        while False:
+            return 7
+        for i in range(3):
+            print(3)
